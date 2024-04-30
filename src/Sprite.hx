0f57eec6c60f241b85b50079e5339203c6bd09cb
SabeDoesThings <122580233+SabeDoesThings@users.noreply.github.com>
2024-04-30 16:39:27 -0500

    Update Sprite.hx

--- src/Sprite.hx
+++ src/Sprite.hx
@@ -2,10 +2,8 @@ import h2d.Anim;
 import hxd.res.Image;
 
 class Sprite {
-    public static function createAnimFromStrip(parent, image_resource: Image, speed, chosenFrames: Array<Int>, centered: Bool = true, lookLeft: Bool = false): Anim {
-        var height = image_resource.getSize().height;
-        var autoCout = Math.floor(image_resource.getSize().width / height);
-        var allFrames = image_resource.toTile().split(autoCout);
+    public static function createAnimFromStrip(parent, image_resource: Image, speed, numOfFrames: Int, chosenFrames: Array<Int>, centered: Bool = true, lookLeft: Bool = false): Anim {
+        var allFrames = image_resource.toTile().split(numOfFrames);
         for (f in allFrames) {
             if (centered)
                 f.setCenterRatio();
